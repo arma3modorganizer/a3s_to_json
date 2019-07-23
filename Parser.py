714d7998663aef36c30873377206b68b613e9ce0
Scarjit <scarjit@aol.com>
2019-07-23 04:15:01 +0200

    Added support for disabling parts of the parser.

--- Parser.py
+++ Parser.py
@@ -10,6 +10,11 @@ if __name__ == "__main__":
     parser.add_argument("url", metavar='url', type=str,
                         help="Examples: ftp://arma.example.com/.a3s/ or http://arma3.example.ru/test/.a3s")
     parser.add_argument("filename", metavar='filename', type=str, help="Output filename")
+    parser.add_argument("--noautoconfig", type=str, help="Don't parse autoconfig")
+    parser.add_argument("--noserverinfo", type=str, help="Don't parse serverinfo")
+    parser.add_argument("--noevents", type=str, help="Don't parse events")
+    parser.add_argument("--nochangelog", type=str, help="Don't parse changelog")
+    parser.add_argument("--nosync", type=str, help="Don't parse sync")
     args = parser.parse_args()
 
     url = args.url
@@ -18,7 +23,14 @@ if __name__ == "__main__":
     scheme: str = parsed_url.scheme
     scheme = scheme.capitalize()
 
-    x = repository.parse(url, scheme)
+    pAutoconfig = True if args.noautoconfig is None else False
+    pServerinfo = True if args.noserverinfo is None else False
+    pEvents = True if args.noevents is None else False
+    pChangelog = True if args.nochangelog is None else False
+    pSync = True if args.nosync is None else False
+
+    x = repository.parse(url, scheme, parseAutoconf=pAutoconfig, parseServerinfo=pServerinfo, parseEvents=pEvents,
+                         parseChangelog=pChangelog, parseSync=pSync)
     x_j = json.dumps(x, indent=True)
 
     with open(args.filename, "w") as f:
